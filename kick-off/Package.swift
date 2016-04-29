327703ee2c92e91f39ed74bd2f99c98a156a9a48
Peter Schumacher <Peter.Schumacher@burda-forward.de>
2016-04-29 19:24:59 +0200

    Added Libs hard for easy xcode builing

--- kick-off/Package.swift
+++ kick-off/Package.swift
@@ -5,8 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/qutheory/vapor-zewo-mustache.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/qutheory/csqlite.git", majorVersion: 0),
-        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0)
+        .Package(url: "https://github.com/FOL-App-Devs/fluent-sqlite.git", majorVersion: 0)
     ],
     exclude: [
         "Deploy",
